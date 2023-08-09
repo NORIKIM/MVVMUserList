6f54fbbf5e8614d0046417b3b2e123adc8cf364b
NORIKIM <jinagold.k@gmail.com>
2023-08-09 22:57:04 +0900

    에러메세지 처리 객체 생성

---
+++ GithubUser/GithubUser/Constants/ErrorMessage.swift
@@ -0,0 +1,23 @@
+//
+//  ErrorMessage.swift
+//  GithubUser
+//
+//  Created by 김지나 on 2023/08/08.
+//
+
+import Foundation
+
+struct ErrorMessage {
+    // 키체인
+    static let saveTokenFail = "토큰 저장에 실패했습니다."
+    static let readTokenFail = "토큰을 읽어오는데 실패했습니다."
+    static let deleteTokenFail = "토큰 삭제에 실패했습니다."
+    
+    // oauth client 값
+    static let readClientIdFail = "client ID를 설정해주세요."
+    static let readClientSecretFail = "client secret을 설정해주세요."
+    
+    // UI
+    static let userCellInitializeFail = "UserCell init에 실패했습니다"
+    static let loadCellInitializeFail = "LoadCell init에 실패했습니다"
+}
