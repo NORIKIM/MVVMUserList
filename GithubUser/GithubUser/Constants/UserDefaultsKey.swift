b31367d3db890b6a474ee5ee753f840032ea37ec
NORIKIM <jinagold.k@gmail.com>
2023-08-09 22:57:41 +0900

    userdefaults 키 값 객체 생성

---
+++ GithubUser/GithubUser/Constants/UserDefaultsKey.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefaultsKey.swift
+//  GithubUser
+//
+//  Created by 김지나 on 2023/08/08.
+//
+
+import Foundation
+
+struct UserDefaultsKey {
+    static let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeforeFlag"
+}
